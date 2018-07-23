66a2ae0b52e8992fffcd2640a6d699e76e0522c3
Manjunath Reddy <manju16832003@gmail.com>
2018-07-23 20:50:10 +0800

    mix project structure

---
+++ lib/elixir_pub_sub.ex
@@ -0,0 +1,18 @@
+defmodule ElixirPubSub do
+  @moduledoc """
+  Documentation for ElixirPubSub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirPubSub.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_pub_sub_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirPubSubTest do
+  use ExUnit.Case
+  doctest ElixirPubSub
+
+  test "greets the world" do
+    assert ElixirPubSub.hello() == :world
+  end
+end
